ad8a06c05debcc76ed0721553480695db42ccd44
Lucas Mendes Martins <martinslmendes@gmail.com>
2026-07-22 10:44:47 -0300

    fix: ReceitaWS model fillable property

--- src/Models/ReceitaWS.php
+++ src/Models/ReceitaWS.php
@@ -2,6 +2,7 @@
 
 namespace MLMendes\LaravelReceitaWS\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
@@ -10,9 +11,11 @@ use Illuminate\Support\Carbon;
 /**
  * @property string $uuid
  * @property string $name
+ * @property string $token
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
+#[Fillable(['name', 'token'])]
 #[Table(name: 'receitaws', key: 'uuid', keyType: 'string', incrementing: false)]
 class ReceitaWS extends Model
 {
